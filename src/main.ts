94185fbd736fff2ba387c40447c5b8e879117014
AddeNordh <nordh.andreas@hotmail.com>
2021-08-26 16:37:49 +0200

    2nd commit

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,67 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+
+import {ECS} from 'aws-sdk';
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+	try {
+		const ecs = new ECS();
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+		const getClusterArns = async (): Promise<ECS.StringList> => {
+			return new Promise<string[]>((resolve, reject) => {
+				ecs.listClusters((error, data) => {
+					if (error) {
+						reject(error);
+						return;
+					}
+					resolve(data.clusterArns);
+				})
+			});
+		};
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+		const getTagsFromResourceArn = async (resourceArn: string): Promise<ECS.Tags> => {
+			return new Promise<ECS.Tags>((resolve, reject) => {
+				ecs.listTagsForResource({resourceArn}, (error, resourceTags) => {
+					if (error) {
+						reject(error);
+						return;
+					}
+
+					resolve(resourceTags.tags);
+				})
+			});
+		};
+
+		const getClusterArnFromTag = async (tagKey: string, tagValue: string): Promise<string> => {
+			let clusterArn: string;
+
+			const clusterArns = await getClusterArns();
+
+			for (const arn of clusterArns) {
+				const resourceTags = await getTagsFromResourceArn(arn);
+
+				resourceTags.map(tag => {
+					if (tag.key === tagKey && tag.value === tagValue) {
+						clusterArn = arn;
+					}
+				});
+			}
+
+			if (!clusterArn) {
+				throw new Error(`No arn found for tag ${tagKey} = ${tagValue}`);
+			}
+
+			return clusterArn;
+		};
+
+		const name = core.getInput('tagName')
+		const value = core.getInput('tagValue')
+
+		const arn = await getClusterArnFromTag(name, value);
+
+		core.setOutput('arn', arn);
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 }
 
-run()
+run();
